305c000e972d14cebf45f8a08f3f7ddef04a6f27
hana67yay <hc.pqu19@manchesterhigh.net>
2026-02-12 10:24:47 +0000

    Set up Flask application with home route

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template("main.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
